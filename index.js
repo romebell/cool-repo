7bc343a077e5b8bf6b1ae6f39b329980abde43ac
Rome Bell <romebell5@gmail.com>
2021-02-12 10:11:29 -0800

    index: Did not get back data???

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ axios.get('https://api.spacexdata.com/v3/missions')
 .then(response => {
     console.log(response.data);
 });
+
+const githubData = async () => {
+    const github = axios.get('https://api.github.com/users/romebell');
+    console.log(github);
+}
+
+// run the function
+githubData();
